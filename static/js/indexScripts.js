5ef11024f94f338c667d0044caa5030f81e836a2
WilfredGichuru <nrwilfred@gmail.com>
2022-04-20 16:50:54 +0300

    first commit

--- static/js/indexScripts.js
+++ static/js/indexScripts.js
@@ -58,9 +58,9 @@ else{
 
 window.onload=animateText;
 
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-e.preventDefault();
-e.target.elements.name.value = '';
-e.target.elements.email.value = '';
-e.target.elements.message.value = '';
-});
+// document.querySelector('#contact-form').addEventListener('submit', (e) => {
+// e.preventDefault();
+// e.target.elements.name.value = '';
+// e.target.elements.email.value = '';
+// e.target.elements.message.value = '';
+// });
